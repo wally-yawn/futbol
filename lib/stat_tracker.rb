a6153301bfceaf303ae863d6383a33734e393b9e
Eric DeShetler <ejdesh@gmail.com>
2024-09-16 16:59:46 -0700

    initial commit

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,16 @@
+class StatTracker
+  def initialize; end
+
+  def self.from_csv(locations)
+    # puts locations
+  end
+
+  def highest_total_score
+    # todo
+  end
+
+  def lowest_total_score
+    # todo
+  end
+end
+
