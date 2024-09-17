4790d60877e5a9f7216e962ae03ad057df185fa3
Wally Wallace <jrvw001@gmail.com>
2024-09-17 10:19:09 -0600

    Create psuedo tests for Stat_tracker.highest_total_score and Stat_tracker.lowest_total_score

---
+++ spec/stat_tracker_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.describe 'stat_tracker' do
+
+
+  describe '#highest_total_score' do
+    it 'can generate the highest total score of a game if there is only one' do
+      expect(true).to eq(false)
+    end
+    it 'can generate the highest total score of a game if there is multiple games with the highest score' do
+      expect(true).to eq(false)
+    end
+  end
+
+  describe '#lowest_total_score' do
+    it 'can generate the lowest total score of a game if there is only one' do
+      expect(true).to eq(false)
+    end
+    it 'can generate the lowest total score of a game if there is multiple games with the highest score' do
+      expect(true).to eq(false)
+    end
+  end
+end
